b4979c93641559a51e44ae4e846dfa82aa7dfa0d
princzny <princzny@gmail.com>
2021-10-28 17:00:09 +0100

    Purple Logo Updated

--- src/components/layout/partials/Logo.js
+++ src/components/layout/partials/Logo.js
@@ -2,14 +2,15 @@ import React from "react";
 import classNames from "classnames";
 import { Link } from "react-router-dom";
 import Image from "../../elements/Image";
-import logo from "./../../../assets/images/logo-2.png";
+// import logo from "./../../../assets/images/logo-2.png";
+import logo from "./../../../assets/images/AWC.png";
 
 
 const Logo = ({ className, ...props }) => {
   const classes = classNames("brand", className);
 
   return (
-    <div {...props} className={classes} style={{background:"white"}}>
+    <div {...props} className={classes} >
       <h1 className="m-0">
         <Link to="/">
           <Image src={logo} alt="Open" width={100} height={100} />
